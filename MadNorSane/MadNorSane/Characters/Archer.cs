2bc68282fe7dc7d0338e413596e49784a82f9410
Buicescu Alexandru <alexbuicescu@gmail.com>
2014-04-05 12:10:31 +0300

    Added players classes

---
+++ MadNorSane/MadNorSane/Characters/Archer.cs
@@ -0,0 +1,25 @@
+﻿using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MadNorSane.Characters
+{
+    class Archer : Player
+    {
+        Archer(World _new_world)
+        {
+            my_world = _new_world;
+            my_body = BodyFactory.CreateRectangle(my_world, 1, 1, 1);
+        }
+
+        public bool atack(String _skill)
+        {
+            
+            return true;
+        }
+    }
+}
---
+++ MadNorSane/MadNorSane/Utilities/VS_Body.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MadNorSane.Utilities
+{
+    class Conversions
+    {
+        float _scale = 30;
+
+        float to_pixels(float meters)
+        {
+            return meters * _scale;
+        }
+        float to_meters(float pixels)
+        {
+            return pixels / _scale;
+        }
+    }
+    class VS_Body
+    {
+        
+    }
+}
